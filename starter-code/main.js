2c5108800e6c6e76068bd56f0198a38d90c6dd8f
adammpolak <adam.polak@citrusbyte.com>
2016-08-11 13:29:23 -0400

    Connected pages and defined cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,10 @@
 console.log("JS file is connected to HTML! Woo!")
+
+//this section defines the cards values
+
+var card1 = "queen"; 
+var card2 = "queen";
+var card3 = "king";
+var card4 = "king";
+
+

081a4de87fd52590100ef2191f8ff1bae4601926
adammpolak <adam.polak@citrusbyte.com>
2016-08-12 00:31:13 -0400

    added javascript card creation

--- starter-code/main.js
+++ starter-code/main.js
@@ -7,9 +7,21 @@ var card2 = "queen";
 var card3 = "king";
 var card4 = "king";
 
-if (card2 === card4){
+/*if (card2 === card4){
 	alert("You found a match!");
 }
 else {
 	alert("Sorry, try again.")
+}*/
+
+var board = document.getElementById("game-board");
+
+var createBoard = function(p){
+	for (x=1;x<p;x+=1){
+		var card = document.createElement("div");
+		card.className = "card"
+		board.appendChild(card)}
+
 }
+
+createBoard(8);

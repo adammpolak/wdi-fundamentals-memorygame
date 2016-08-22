6f55475e744766dfef22f1d5cc59ad0a25752db9
adammpolak <adam.polak@citrusbyte.com>
2016-08-22 06:32:58 -0700

    done without resizing or second flip

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,26 +2,64 @@ console.log("JS file is connected to HTML! Woo!")
 
 //this section defines the cards values
 
-var card1 = "queen"; 
-var card2 = "queen";
-var card3 = "king";
-var card4 = "king";
 
-/*if (card2 === card4){
-	alert("You found a match!");
-}
-else {
-	alert("Sorry, try again.")
-}*/
-
+var cards = ["queen","queen","king","king"]
+var cardsInPlay = []
 var board = document.getElementById("game-board");
 
-var createBoard = function(p){
-	for (x=1;x<p;x+=1){
-		var card = document.createElement("div");
-		card.className = "card"
-		board.appendChild(card)}
-
+var flipCard = function(){
+	if (this.getAttribute('data-card')==="king") {
+	this.innerHTML = '<img src="king.png" alt="King of Spades" />'
+}
+else {
+	this.innerHTML = '<img src="queen.jpg" alt="Queen of Hearts" />'
+}
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(5)
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];		
+	}
 }
 
-createBoard(8);
+var clearCards = function(){
+	console.log(7)
+	for (x=0;x<cards.length;x+=1){
+		document.getElementsByClassName("card")[x].innerHTML = ''
+		
+	}
+}
+
+// var isTwoCards = function(){
+// 	if (cardsInPlay.length === 2) {
+// 		isMatch(cardsInPlay);
+// 		cardsInPlay = [];
+// 		console.log(4)
+		
+// 	}
+// }
+
+var isMatch = function(x){
+	if (x[0]===x[1]){
+		window.confirm("Congratulations, it's a match!");
+		clearCards()
+	}
+	else {
+		window.confirm("Sorry, it isn't a match.");
+		clearCards()
+	}
+}
+
+var createBoard = function(){
+	for (x=0;x<cards.length;x+=1){
+		var card = document.createElement("div");
+		card.className = "card";
+		card.addEventListener('click', flipCard)
+		card.setAttribute('data-card', cards[x]);
+//		card.addEventListener('click', isTwoCards)		
+		board.appendChild(card);}
+
+}
+createBoard();
+
+
